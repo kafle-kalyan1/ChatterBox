6cf46460c6e23aea0c8d1f18c6247ec4cd6d7b01
Kalyan <Kalyankafle123@gmail.com>
2023-04-11 21:48:46 +0545

    Last Commit

--- backend/chats/consumers.py
+++ backend/chats/consumers.py
@@ -8,23 +8,25 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.receiver_username = self.scope['url_route']['kwargs']['receiver_username']
 
         # join room group based on sender and receiver usernames
-        self.sender_room_name = f"{self.sender_username}_{self.receiver_username}"
-        self.receiver_room_name = f"{self.receiver_username}_{self.sender_username}"
+        self.room_name = f"{self.sender_username}_{self.receiver_username}"
+        self.room_group_name = f"chat_{self.room_name}"
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
-        self.sender_room_group_name = f"chat_{self.receiver_username}_{self.sender_username}"
-        self.receiver_room_group_name = f"chat_{self.receiver_room_name}"
-
-        await self.channel_layer.group_add(self.sender_room_group_name, self.channel_name)
+        # join room group for receiver as well
+        self.receiver_room_group_name = f"chat_{self.receiver_username}"
         await self.channel_layer.group_add(self.receiver_room_group_name, self.channel_name)
 
         await self.accept()
 
     async def disconnect(self, close_code):
         # leave room group
-        await self.channel_layer.group_discard(self.sender_room_group_name, self.channel_name)
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
         await self.channel_layer.group_discard(self.receiver_room_group_name, self.channel_name)
 
     async def receive(self, text_data):
+        print(f'Group......{self.room_group_name}')
+        print(f'Channels inside the group: {await self.channel_layer.group_channels(self.room_group_name)}')
+        
         # receive message from WebSocket
         message = json.loads(text_data)
         sender = message['sender']
@@ -33,17 +35,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # send message to room group
         await self.channel_layer.group_send(
-            self.sender_room_group_name,
-            {
-                'type': 'chat_message',
-                'sender': sender,
-                'receiver': receiver,
-                'text': text
-            }
-        )
-
-        await self.channel_layer.group_send(
-            self.receiver_room_group_name,
+            self.room_group_name,
             {
                 'type': 'chat_message',
                 'sender': sender,
@@ -61,4 +53,4 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'sender': sender,
             'receiver': receiver,
             'text': text
-        }))
+        })) 
